64cbb3e006e7b8615853f92d8d31c52160a02e5f
Shakti Prakash <shaktiprakash099@gmail.com>
2023-07-01 20:32:37 +0530

    Initial Commit

---
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by Shakti Prakash Srichandan on 30/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISample/SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by Shakti Prakash Srichandan on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
